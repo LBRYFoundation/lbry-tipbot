366de08922a9a582449ed095cd9a7c0460e79ed0
Niko Storni <niko@storni.info>
2018-02-22 01:02:35 +0100

    cleaned code up
    used more of the configuration
    removed useless code
    converted vars into lets
    updated packages
    updated precommit hooks

--- bot/modules/tipbot.js
+++ bot/modules/tipbot.js
@@ -2,90 +2,96 @@
 
 const bitcoin = require('bitcoin');
 let config = require('config');
-config = config.get('lbrycrd');
-const lbry = new bitcoin.Client(config);
+let lbrycrdConfig = config.get('lbrycrd');
+let sandboxChannel = config.get('sandboxchannel');
+const lbry = new bitcoin.Client(lbrycrdConfig);
 
-exports.commands = [
-  "tip"
-]
+exports.commands = ['tip'];
 exports.tip = {
-  usage: "<subcommand>",
-  description: 'balance: get your balance\n    deposit: get address for your deposits\n    withdraw ADDRESS AMOUNT: withdraw AMOUNT credits to ADDRESS\n    [private] <user> <amount>: mention a user with @ and then the amount to tip them, or put private before the user to tip them privately.\n Key: [] : Optionally include contained keyword, <> : Replace with appropriate value.',
-  process: async function (bot, msg, suffix) {
+  usage: '<subcommand>',
+  description: `balance: get your balance
+    deposit: get address for your deposits
+    withdraw ADDRESS AMOUNT: withdraw AMOUNT credits to ADDRESS
+    [private] <user> <amount>: mention a user with @ and then the amount to tip them, or put private before the user to tip them privately.
+ Key: [] : Optionally include contained keyword, <> : Replace with appropriate value.`,
+  process: async function(bot, msg, suffix) {
     let tipper = msg.author.id.replace('!', ''),
-      words = msg.content.trim().split(' ').filter(function (n) { return n !== ""; }),
+      words = msg.content
+        .trim()
+        .split(' ')
+        .filter(function(n) {
+          return n !== '';
+        }),
       subcommand = words.length >= 2 ? words[1] : 'help';
     switch (subcommand) {
-      case 'help': doHelp(msg); break;
-      case 'balance': doBalance(msg, tipper); break;
-      case 'deposit': doDeposit(msg, tipper); break;
-      case 'withdraw': doWithdraw(msg, tipper, words); break;
-      default: doTip(msg, tipper, words);
+      case 'help':
+        doHelp(msg);
+        break;
+      case 'balance':
+        doBalance(msg, tipper);
+        break;
+      case 'deposit':
+        doDeposit(msg, tipper);
+        break;
+      case 'withdraw':
+        doWithdraw(msg, tipper, words);
+        break;
+      default:
+        doTip(msg, tipper, words);
     }
   }
-}
+};
 
 function doBalance(message, tipper) {
-  lbry.getBalance(tipper, 1, function (err, balance) {
+  lbry.getBalance(tipper, 1, function(err, balance) {
     if (err) {
       message.reply('Error getting balance');
-    }
-    else {
+    } else {
       message.reply('You have *' + balance + '* LBC');
     }
   });
 }
 
-
 function doDeposit(message, tipper) {
   if (!inPrivateOrBotSandbox(message)) {
-    message.reply('Please use <#369896313082478594> or DMs to talk to bots.');
-    return;
+    return message.reply(`Please use <#${sandboxChannel}> or DMs to talk to bots.`);
   }
-  getAddress(tipper, function (err, address) {
+  getAddress(tipper, function(err, address) {
     if (err) {
       message.reply('Error getting deposit address');
-    }
-    else {
+    } else {
       message.reply('Your address is ' + address);
     }
   });
 }
 
-
 function doWithdraw(message, tipper, words) {
   if (!inPrivateOrBotSandbox(message)) {
-    message.reply('Please use <#369896313082478594> or DMs to talk to bots.');
-    return;
+    return message.reply(`Please use <#${sandboxChannel}> or DMs to talk to bots.`);
   }
   if (words.length < 4) {
-    doHelp(message);
-    return;
+    return doHelp(message);
   }
 
-  var address = words[2],
+  let address = words[2],
     amount = getValidatedAmount(words[3]);
 
   if (amount === null) {
-    message.reply('I dont know how to withdraw that many credits');
+    message.reply("I don't know how to withdraw that many credits");
     return;
   }
 
-  lbry.sendFrom(tipper, address, amount, function (err, txId) {
+  lbry.sendFrom(tipper, address, amount, function(err, txId) {
     if (err) {
-      message.reply(err.message);
+      return message.reply(err.message);
     }
-    else {
-      message.reply('You withdrew ' + amount + ' to ' + address + ' (' + txLink(txId) + ')');
-    }
+    message.reply(`You withdrew ${amount} to ${address} (${txLink(txId)})`);
   });
 }
 
-
 function doTip(message, tipper, words) {
   if (words.length < 3 || !words) {
-    doHelp(message);
-    return;
+    return doHelp(message);
   }
 
   let prv = 0;
@@ -98,44 +104,40 @@ function doTip(message, tipper, words) {
   let amount = getValidatedAmount(words[amountOffset]);
 
   if (amount === null) {
-    message.reply('I dont know how to tip that many credits');
-    return;
+    return message.reply("I don't know how to tip that many credits");
   }
 
   if (message.mentions.users.first().id) {
-    sendLbc(message, tipper, message.mentions.users.first().id.replace('!', ''), amount, prv);
+    return sendLbc(message, tipper, message.mentions.users.first().id.replace('!', ''), amount, prv);
   }
-  else {
-    message.reply('Sorry, I could not find a user in your tip...');
-  }
+  message.reply('Sorry, I could not find a user in your tip...');
 }
-
 
 function doHelp(message) {
   if (!inPrivateOrBotSandbox(message)) {
     message.reply('Sent you help via DM! Please use <#369896313082478594> or DMs to talk to bots.');
   }
-  message.author.send('**!tip**\n    balance: get your balance\n    deposit: get address for your deposits\n    withdraw ADDRESS AMOUNT: withdraw AMOUNT credits to ADDRESS\n    [private] <user> <amount>: mention a user with @ and then the amount to tip them, or put private before the user to tip them privately.\n    Key: [] : Optionally include contained keyword, <> : Replace with appropriate value.');
+  message.author.send(`**!tip**
+    balance: get your balance
+    deposit: get address for your deposits
+    withdraw ADDRESS AMOUNT: withdraw AMOUNT credits to ADDRESS
+    [private] <user> <amount>: mention a user with @ and then the amount to tip them, or put private before the user to tip them privately.
+    Key: [] : Optionally include contained keyword, <> : Replace with appropriate value.`);
 }
 
-
 function sendLbc(message, tipper, recipient, amount, privacyFlag) {
-  getAddress(recipient, function (err, address) {
+  getAddress(recipient, function(err, address) {
     if (err) {
       message.reply(err.message);
-    }
-    else {
-      lbry.sendFrom(tipper, address, amount, 1, null, null, function (err, txId) {
+    } else {
+      lbry.sendFrom(tipper, address, amount, 1, null, null, function(err, txId) {
         if (err) {
           message.reply(err.message);
-        }
-        else {
-          var imessage =
-            'Wubba lubba dub dub! <@' + tipper + '> tipped <@' + recipient + '> ' + amount + ' LBC (' + txLink(txId) + '). ' +
-            'DM me `!tip` for tipbot instructions.'
+        } else {
+          let imessage = `Wubba lubba dub dub! <@${tipper}> tipped <@${recipient}> ${amount} LBC (${txLink(txId)}). DM me \`!tip\` for tipbot instructions.`;
           if (privacyFlag) {
             message.author.send(imessage);
-            if (message.author.id != message.mentions.users.first().id) {
+            if (message.author.id !== message.mentions.users.first().id) {
               message.mentions.users.first().send(imessage);
             }
           } else {
@@ -145,23 +147,19 @@ function sendLbc(message, tipper, recipient, amount, privacyFlag) {
       });
     }
   });
-};
-
+}
 
 function getAddress(userId, cb) {
-  lbry.getAddressesByAccount(userId, function (err, addresses) {
+  lbry.getAddressesByAccount(userId, function(err, addresses) {
     if (err) {
       cb(err);
-    }
-    else if (addresses.length > 0) {
+    } else if (addresses.length > 0) {
       cb(null, addresses[0]);
-    }
-    else {
-      lbry.getNewAddress(userId, function (err, address) {
+    } else {
+      lbry.getNewAddress(userId, function(err, address) {
         if (err) {
           cb(err);
-        }
-        else {
+        } else {
           cb(null, address);
         }
       });
@@ -170,11 +168,7 @@ function getAddress(userId, cb) {
 }
 
 function inPrivateOrBotSandbox(msg) {
-  if ((msg.channel.type == 'dm') || (msg.channel.id === '369896313082478594')) {
-    return true;
-  } else {
-    return false;
-  }
+  return msg.channel.type === 'dm' || msg.channel.id === sandboxChannel;
 }
 
 function getValidatedAmount(amount) {
@@ -185,7 +179,6 @@ function getValidatedAmount(amount) {
   return amount.match(/^[0-9]+(\.[0-9]+)?$/) ? amount : null;
 }
 
-
 function txLink(txId) {
-  return "<https://explorer.lbry.io/tx/" + txId + ">";
+  return '<https://explorer.lbry.io/tx/' + txId + '>';
 }
